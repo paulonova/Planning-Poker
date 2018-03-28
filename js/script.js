fcc43305194815a0686b59ecb956cdbd7ae6c3f5
paulonova <paulonova144@gmail.com>
2018-03-28 20:43:03 +0200

    Select and deselect cards

--- js/script.js
+++ js/script.js
@@ -1,37 +1,28 @@
 
-
-let cards = ["0", "1/2", "1","2","3","5","8","13","20","40", "?", "&#x2615;"];
-
-let users = [];
-
-let playerName;
-
+/**VARIABLES ***/
+var cards = ["0", "1/2", "1","2","3","5","8","13","20","40", "?", "&#x2615;"];
+var isCardSelected = false;
+var users = [];
+var playerName;
 var player;
-
-let selectedCard = null;
-
-let gameState = false;
-
-var playerSelected = false;
+var selectedCard = null;
+var gameState = false;
+var isPlayerSelected = false;
 
 
-/*ELEMENTS ***/
+/**ELEMENTS ***/
 let deck = document.getElementById('deck');
-
 let score = document.getElementById('score');
-
 let finishBtn = document.getElementById('finish');
-
 let clearBtn = document.getElementById('clear');
-
 let addPlayerBtn = document.getElementById('add-player');
-
 let nameInput = document.getElementById('name');
 
 
 class Player{
-    constructor(playerName){
+    constructor(playerName, playerSelected){
         this.playerName = playerName;
+        this.isPlayerSelected = playerSelected;
     }
 
     setPlayerSelected(status){
@@ -39,13 +30,16 @@ class Player{
     }
 
     getPlayerSelected(){
-        return this.playerSelected;
+        return this.isPlayerSelected;
     }
 }
 
 
+
+
 function addCard(element, value, name){
     let cardElement = document.createElement("DIV");
+    console.log("ADD_CARD: " + element + " value: " + value + " Name: " + name);
 
     if(name){
         cardElement.className = value ? 'card card-user card-ready' : 'card card-user';
@@ -56,26 +50,65 @@ function addCard(element, value, name){
         cardElement.innerHTML = '<div class="card_inner">' + value + (name ? '<div class="card_name">' 
             + name + '</div>' : '') + '</div>';
     }
+    
 
+    /**CARD EVENT LISTENER ***/
     cardElement.addEventListener("click", function(){
         console.log("Card EVENT: " + value);
-        selectCard(value);
+        console.log("cardSelected0: " + isCardSelected);
+
+        if(!isCardSelected){
+            selectCard(value);
+            console.log("cardSelected1: " + isCardSelected);                       
+    
+        }else{
+
+            if(name){
+                cardElement.className = value ? 'card card-user card-ready' : 'card card-user';
+                cardElement.innerHTML = '<div class="card_inner">' + (gameState ? value : '') 
+                    + (name ? '<div class="card_name">' + name + '</div>' : '') + '</div>';
+            }else{
+                cardElement.className = value == selectedCard ? 'card card-unselected' : 'card';
+                cardElement.innerHTML = '<div class="card_inner">' + value + (name ? '<div class="card_name">' 
+                    + name + '</div>' : '') + '</div>';
+            }
+            console.log("cardSelected2: " + isCardSelected);
+            isCardSelected = false;
+        }
+
     })
 
     element.appendChild(cardElement);
+}
+
+/**Card click */
+function selectCard(card){
+    
+    if(isPlayerSelected){
+        selectedCard = card;
+        isCardSelected = true;
+        render();
+    }else{
+        alert("Create first a player..");
+        isCardSelected = false;
+    }
+
 }
 
 
 /** AddPlayer EVENT*/
 addPlayerBtn.addEventListener("click", function(){
     playerName = nameInput.value;
-    player = new Player(playerName);
+    
+    player = new Player(playerName, false);
 
-    if(player.playerName){
+    if(player.playerName || playerName != ""){
         console.log("AddPlayer: " + player.playerName);
+        isPlayerSelected = true;  
         player.setPlayerSelected(true);
         nameInput.value = "";
     }else{
+        isPlayerSelected = false;
         alert("Please enter your name");
     }
     console.log("Player Selected: " + player.getPlayerSelected());
@@ -83,25 +116,12 @@ addPlayerBtn.addEventListener("click", function(){
 });
 
 
-function selectCard(card){
-
-    /**Preciso  pegar o playerSelect com valores atuais..*/
-
-    console.log("Testing PlayerSelected: " + playerSelected)
-    if(player.playerName != "undefined"){
-        selectedCard = card;
-        render();
-    }else{
-        alert("Something wrong");
-    }
-    
-}
-
 
 /**RENDER CARDS *** */
 function renderCards(){
     deck.innerHTML = "";
     cards.forEach((item, index) => {
+        // addCard(deck, item, "playerName");
         addCard(deck, item);
     });
 }
